5de1f4d72898a7d21f18c2f498b8450e56506717
seanpmaxwell <spmaxwell7@gmail.com>
2024-10-22 18:32:57 -0700

    switched to tsx

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import logger from 'jet-logger';
-import EnvVars from '@src/common/EnvVars';
-import server from './server';
-var SERVER_START_MSG = ('Express server started on port: ' +
-    EnvVars.Port.toString());
-server.listen(EnvVars.Port, function () { return logger.info(SERVER_START_MSG); });
