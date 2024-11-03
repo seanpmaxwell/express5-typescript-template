f44ccaea62e2efb3d452c9d1d64e0768d9885c36
seanpmaxwell <spmaxwell7@gmail.com>
2024-11-03 10:24:54 -0800

    renamed fn to vf

--- src/util/schema.ts
+++ src/util/schema.ts
@@ -4,8 +4,8 @@ import { isBool, isNum, isStr } from './validators';
 
 export default jetSchema({
   globals: [
-    { fn: isNum, default: 0 },
-    { fn: isStr, default: '' },
-    { fn: isBool, default: false },
+    { vf: isNum, default: 0 },
+    { vf: isStr, default: '' },
+    { vf: isBool, default: false },
   ],
 });
